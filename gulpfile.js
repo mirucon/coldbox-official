5265c3fe46f0c2ce9bc7969479f183d3d1d50b1f
mirucon <i@miruc.co>
2018-06-24 11:53:46 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,5 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var pug = require('gulp-pug')
-var browserSync = require('browser-sync');
-var autoprefixer = require('gulp-autoprefixer');
 var postcss = require('gulp-postcss');
 var cssnext = require('postcss-cssnext');
 var cssmin = require('gulp-cssmin');
@@ -11,13 +8,6 @@ var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
 var rename = require('gulp-rename');
 
-gulp.task("pug", function() {
-    gulp.src(["pug/*.pug"])
-  .pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))
-    .pipe(pug({pretty: true}))
-    .pipe(rename({ extname: '.php' }))
-    .pipe(gulp.dest("."))
-});
 gulp.task( 'minify', function () {
   gulp.src('style.css')
   .pipe(cssmin())
@@ -27,32 +17,17 @@ gulp.task( 'minify', function () {
 
 gulp.task('sass', function() {
   var processors = [
-    cssnext({browsers: ['last 2 version', 'iOS 8.4'], flexbox: 'no-2009'})
+    cssnext({browsers: ['last 2 version'], flexbox: 'no-2009'})
   ];
   return gulp.src(['sass/*.scss'])
-  .pipe(sourcemaps.init())
-  .pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))
-  .pipe(sass({outputStyle: 'expanded',}))
-  .pipe(postcss(processors))
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest('.'));
+    .pipe(sourcemaps.init())
+    .pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))
+    .pipe(sass({outputStyle: 'expanded',}))
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('.'));
 });
 
-gulp.task('browser-sync', function () {
-  browserSync({
-    open: 'external',
-    notify: false,
-    proxy: "http://official-coldbox.dev/",
-    port: "8000",
-  });
-});
-gulp.task('bs-reload', function () {
-  browserSync.reload();
-});
-
-gulp.task('default', ['browser-sync'], function () {
-  gulp.watch("sass/*.scss", ['sass', 'bs-reload']);
-  gulp.watch("pug/*.pug", ['pug', 'bs-reload']);
-  gulp.watch("js/*.*", ['bs-reload']);
-  gulp.watch("*.*", ['bs-reload']);
+gulp.task('default', function () {
+  gulp.watch("sass/*.scss", ['sass', 'minify']);
 });
